e0cbd82086965122bcc3f121eb8b2fc11364870b
Phonepasert <tradtrarist@gmail.com>
2025-01-08 15:46:30 +0700

    Delete demo.py

--- demo.py
+++
@@ -1,9 +0,0 @@
-from Student import Student
-
-class Phone(Student):
-    def name(self):
-        return "Phone"
-    def speak(self):
-        return "Toi len la: Phone \nEmail: tradtrarist@gmail.com"
-    def address(self):
-        return "DH city"
